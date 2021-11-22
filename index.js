3285ae7f2680adca72818c2408f9cd43c62e1492
TaylorDarneille <tmdarneille@gmail.com>
2021-11-22 07:43:06 -0800

    for deployment

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/profile', isLoggedIn, (req, res)=>{
 })
 
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log(`process.env.SUPER_SECRET_SECRET ${process.env.SUPER_SECRET_SECRET}`)
     console.log("auth_practice running on port 3000")
 })
